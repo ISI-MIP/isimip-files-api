b9a4cea3974001fa08541a91db630b79adadeb97
Jochen Klar <mail@jochenklar.de>
2020-06-04 18:11:34 +0200

    Update worker logging

--- isimip_files_api/worker.py
+++ isimip_files_api/worker.py
@@ -4,7 +4,8 @@ from rq import Worker as Worker
 
 from .settings import WORKER_LOG_FILE, WORKER_LOG_LEVEL
 
-logging.basicConfig(level=WORKER_LOG_LEVEL, filename=WORKER_LOG_FILE)
+logging.basicConfig(level=WORKER_LOG_LEVEL, filename=WORKER_LOG_FILE,
+                    format='[%(asctime)s] %(levelname)s %(name)s: %(message)s')
 
 
 class LogWorker(Worker):
