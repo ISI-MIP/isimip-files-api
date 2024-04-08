593bae92ed2c17d292a4c6e06febe1e197adf9d1
Jochen Klar <mail@jochenklar.de>
2024-04-08 13:23:36 +0200

    Use structlog in examples

--- examples/mask_bbox.py
+++ examples/mask_bbox.py
@@ -1,9 +1,12 @@
-import json
 import time
 import zipfile
 from pathlib import Path
 
+# install required dependencies with `pip install requests structlog`
 import requests
+import structlog
+
+log = structlog.get_logger()
 
 url = 'https://files.isimip.org/api/v2'
 
@@ -36,9 +39,9 @@ response = requests.post(url, json=data)
 # extract the job object from the response
 try:
     job = response.json()
-    print(json.dumps(job, indent=2))
+    log.info('job submitted', id=job['id'], status=job['status'])
 except requests.exceptions.JSONDecodeError:
-    print(response.text)
+    log.info('job submission failed', error=response.text)
 else:
     while job['status'] in ['queued', 'started']:
         # wait for 4 sec
@@ -46,12 +49,13 @@ else:
 
         # check the status of the job
         job = requests.get(job['job_url']).json()
-        print(json.dumps(job, indent=2))
+        log.info('job updated', id=job['id'], status=job['status'], meta=job['meta'])
 
     if job['status'] == 'finished':
         # download file
         zip_path = Path(download_path) / job['file_name']
         zip_path.parent.mkdir(exist_ok=True)
+        log.info('downloading', file_url=job['file_url'])
         with requests.get(job['file_url'], stream=True) as response:
             with zip_path.open('wb') as fp:
                 for chunk in response.iter_content(chunk_size=8192):
@@ -60,5 +64,6 @@ else:
         # extract zip file
         out_path = zip_path.with_suffix('')
         out_path.mkdir(exist_ok=True)
+        log.info('extracting', zip_path=str(out_path.resolve()))
         with zipfile.ZipFile(zip_path, 'r') as zf:
             zf.extractall(out_path)
